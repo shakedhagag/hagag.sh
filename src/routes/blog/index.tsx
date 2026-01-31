7703963cfeeb7f4407029d5c9649505db4a16103
Shaked Hagag <shaked@frame.security>
2026-01-31 19:41:43 +0200

    quote

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -28,6 +28,22 @@ function BlogIndex() {
   return (
     <div className="mx-auto max-w-3xl px-5 py-12">
       <Header />
+      <blockquote className="mb-8 border-l-2 pl-4 font-semibold text-foreground/65 text-lg italic dark:text-foreground/75">
+        <svg
+          className="mb-2 h-4 w-4 text-muted-foreground/60"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 14"
+        >
+          <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+        </svg>
+        <p className="font-thin">
+          At each stage do whatever seems most interesting and gives you the
+          best options for the future. I call this approach "staying upwind."
+          This is how most people who've done great work seem to have done it.
+        </p>
+      </blockquote>
       <h2 className="font-bold text-foreground/45 text-sm uppercase leading-loose tracking-wider">
         Recent Posts
       </h2>
